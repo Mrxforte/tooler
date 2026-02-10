b289250f34218282bd9f942b287b340e138b9df3
MrxForte <urazimbetovazamat6@gmail.com>
2026-02-10 08:05:12 -0800

    wokring

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tooler/main1.dart';
+import 'package:tooler/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
